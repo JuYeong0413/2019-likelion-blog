c78b53b9848f467ec699ce596b0dbe76435e9d2d
JuYeong0413 <2jy22@naver.com>
2019-07-08 00:00:52 +0900

    blog app url

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('<int:blog_id>', views.detail, name="detail"),
+    # path converter : 여러 객체들을 다루는, 계층적인 url을 자동생성할 때 유리함
+    path('new/', views.new, name="new"),
+    path('create/', views.create, name="new"),
+]
--- blog_project/urls.py
+++ blog_project/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import blog.views
 import portfolio.views
 from django.conf import settings
@@ -23,9 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.home, name="home"),
-    path('blog/<int:blog_id>', blog.views.detail, name="detail"),
-    # path converter : 여러 객체들을 다루는, 계층적인 url을 자동생성할 때 유리함
-    path('blog/new/', blog.views.new, name="new"),
-    path('blog/create/', blog.views.create, name="new"),
+    path('blog/', include(blog.urls)),
     path('portfolio/', portfolio.views.portfolio, name="portfolio"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
