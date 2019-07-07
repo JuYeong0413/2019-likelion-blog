eb4fac6d087a7d3e960c95ebdc67cee2bb791b39
JuYeong0413 <2jy22@naver.com>
2019-07-08 00:38:14 +0900

    signup function

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 def signup(request):
+    if request.method == "POST":
+        if request.POST.['password1'] == request.POST['password2']:
+            user = User.objects.create_user(username=request.POST['username'], password=request.POST.['password1'])
+            auth.login(request, user)
+            return redirect('home')
+
     return render(request, 'accounts/signup.html')
 
 
