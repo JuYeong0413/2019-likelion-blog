128322b82fea94962a1511961592b8e9abd47320
JuYeong0413 <2jy22@naver.com>
2019-07-08 00:02:21 +0900

    fix error

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -23,6 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog.views.home, name="home"),
-    path('blog/', include(blog.urls)),
+    path('blog/', include('blog.urls')),
     path('portfolio/', portfolio.views.portfolio, name="portfolio"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
