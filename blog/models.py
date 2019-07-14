0a194e407e3ba056d71d2046f4774945b1c0572c
JuYeong0413 <2jy22@naver.com>
2019-07-14 16:00:51 +0900

    add writer to blog model

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     body = models.TextField()
+    writer = models.ForeignKey(User, on_delete="CASCADE")
+    
 
     def __str__(self):
         return self.title
